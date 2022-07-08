06a91bd0e4efccb4ceee3a14f643e3feb56eca69
Sciman101 <Sciman1011@Gmail.com>
2022-07-07 21:29:58 -0400

    Added lowDB

--- index.js
+++ index.js
@@ -7,10 +7,12 @@ const {prefix, token} = require('./config.json');
 
 import Babbler from './util/babbler.js';
 import Logger, {log} from './util/logger.js';
+import Database, {db} from './util/db.js';
 import argumentParser from './parser/argumentParser.js';
 
 Logger.init();
-;Babbler.init();
+Database.init();
+Babbler.init();
 
 // Setup discord client
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MESSAGE_REACTIONS, Intents.FLAGS.DIRECT_MESSAGES, Intents.FLAGS.DIRECT_MESSAGE_REACTIONS] });
--- util/db.js
+++ util/db.js
@@ -1,14 +1,36 @@
 import {Low, JSONFile} from 'lowdb';
 import {log} from './logger.js';
 
-export default{
+const SCHEDULED_WRITE_TIMEOUT = 5000;
+
+export let db;
+
+let writeQueued = false;
+
+export default {
 
 	init: async () => {
-
-		const db = new Low(new JSONFile('db.json'));
-		await db.read();
+		const _db = new Low(new JSONFile('db.json'));
+		await _db.read();
 		log.info('Database initialized!');
+		db = _db;
 
+		db.data ||= {
+			users: [],
+			guilds: [],
+			jnfr: {}
+		}
+	},
+
+	scheduleWrite: () => {
+		if (!writeQueued) {
+			log.info('Writing database contents in ',SCHEDULED_WRITE_TIMEOUT,'ms');
+			writeQueued = true;
+			setTimeout(() => {
+				db.write();
+				log.info('Database contents written');
+			},SCHEDULED_WRITE_TIMEOUT)
+		}
 	}
 
 }
