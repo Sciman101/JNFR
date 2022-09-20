217c348b405129881dde632af194e3b0d9110dd2
Sciman101 <Sciman1011@Gmail.com>
2022-09-19 22:28:45 -0400

    made duckduckbang ignore !!!

--- commands/duckduckbang.js
+++ commands/duckduckbang.js
@@ -1,30 +1,30 @@
 import { duckIt } from "node-duckduckgo";
 
 export default {
-	name: 'duckduckbang',
-	description: 'Adds support for DuckDuckGo\'s bang! search within discord',
-	args:null,
-	guildOnly:false,
-	hidden:true,
-	execute(message, args) {	
-		message.reply("The full list of available bangs can be found at https://duckduckgo.com/bang");
-	},
-	listeners:{
-		'messageCreate':function(message) {
-			// Ignore certain messages
-			if (message.author.bot || !message.content.startsWith('!!')) return;
+    name: 'duckduckbang',
+    description: 'Adds support for DuckDuckGo\'s bang! search within discord',
+    args: null,
+    guildOnly: false,
+    hidden: true,
+    execute(message, args) {
+        message.reply("The full list of available bangs can be found at https://duckduckgo.com/bang");
+    },
+    listeners: {
+        'messageCreate': function(message) {
+            // Ignore certain messages
+            if (message.author.bot || !message.content.startsWith('!!') || message.content.startsWith('!!!')) return;
 
-			// Format is !(bang) - ex. !!w mario
-			doBangSearch(message.content.substring(1),message);
-		}
-	}
+            // Format is !(bang) - ex. !!w mario
+            doBangSearch(message.content.substring(1), message);
+        }
+    }
 }
 
-async function doBangSearch(query,message) {
-	const response = await duckIt(query);
-	if (response.status == 200 && response.request.res && response.request.res.responseUrl && !response.request.res.responseUrl.startsWith("https://duckduckgo.com")) {
-		return message.reply(response.request.res.responseUrl);
-	}else{
-		message.react('🚫');
-	}
+async function doBangSearch(query, message) {
+    const response = await duckIt(query);
+    if (response.status == 200 && response.request.res && response.request.res.responseUrl && !response.request.res.responseUrl.startsWith("https://duckduckgo.com")) {
+        return message.reply(response.request.res.responseUrl);
+    } else {
+        message.react('🚫');
+    }
 }
