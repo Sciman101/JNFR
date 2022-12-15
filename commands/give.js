3be936bc89fdac9a35e1f0f8c83c03e159dcf28c
Sciman101 <Sciman1011@Gmail.com>
2022-12-14 19:17:20 -0500

    Improved j!give functionality

--- commands/give.js
+++ commands/give.js
@@ -20,37 +20,44 @@ export default {
 
 		// Figure out what to give them
 		const gift = args.gift;
-		const jollarAmount = parseInt(gift);
-
-		// Find an item
-		let item = searchInventory(userData.inventory,gift);
-
-		if (item.length > 0) {
-			item = item[0];
-			// give it			
-			addItem(gifteeData,item.id,1);
-			removeItem(userData,item.id,1);
-
-			Database.scheduleWrite();
-
-			return message.reply(`You gave away your '${items[item.id].name}'!`);
-		}else if (jollarAmount) {
+		if (gift.toLowerCase().endsWith('jlr') || gift.toLowerCase().endsWith(' jlr')) {
+			const jollarAmount = parseInt(gift.substring(0,gift.length - 3));
 			// Do we have that much?
+			if (jollarAmount > 0) {
+				if (userData.balance >= jollarAmount) {
 
-			if (userData.balance >= jollarAmount) {
+					userData.balance -= jollarAmount;
+					gifteeData.balance += jollarAmount;
 
-				userData.balance -= jollarAmount;
-				gifteeData.balance += jollarAmount;
+					Database.scheduleWrite();
+
+					return message.reply(`You gave away ${jollarAmount}${Babbler.getJollarSign(message.guild)}!`);
+
+				}else{
+					return message.reply("You don't have that many Jollars!");
+				}
+			}else{
+				return message.reply("You can't give that amount of Jollars!");
+			}
+		}else{
+			// Find an item
+			let item = searchInventory(userData.inventory,gift);
+
+			if (item.length == 1) {
+				item = item[0];
+				// give it			
+				addItem(gifteeData,item.id,1);
+				removeItem(userData,item.id,1);
 
 				Database.scheduleWrite();
 
-				return message.reply(`You gave away ${jollarAmount}${Babbler.getJollarSign(message.guild)}!`);
-
+				return message.reply(`You gave away your '${items[item.id].name}'!`);
+			}else if (item.length > 1) {
+				const list = item.map(itm => items[itm.id]).reduce((text,itm) => `${text}\n-${itm.name}`,'Matching items: ');
+				return message.reply(`You have multiple items that match, be more specific!\n${list}\n(If you want to send jollars, add 'JLR' to the end of the number)`);
 			}else{
-				return message.reply("You don't have that many Jollars!");
+				return message.reply("You don't have that to give!\n(If you want to send jollars, add 'JLR' to the end of the number)");
 			}
-		}else{
-			return message.reply("You don't have that to give!");
 		}
 
 	}
