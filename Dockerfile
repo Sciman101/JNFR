fe275b096904027ca5fcc4e40bbaa20192286eb4
Sciman101 <Sciman1011@Gmail.com>
2023-11-10 21:41:58 -0500

    FUCK Docker

--- Dockerfile
+++
@@ -1,8 +0,0 @@
-# syntax=docker/dockerfile:1
-
-FROM node:18-alpine
-WORKDIR /app
-COPY package*.json .
-RUN npm ci
-COPY . .
-CMD ["node", "index.js"]
--- util/db.js
+++ util/db.js
@@ -1,74 +1,65 @@
-import {Low, JSONFile} from 'lowdb';
-import {log} from './logger.js';
-import fs from 'fs';
+import { Low, JSONFile } from "lowdb";
+import { log } from "./logger.js";
 
 const SCHEDULED_WRITE_TIMEOUT = 5000;
-const DB_PATH = 'storage/db.json';
+const DB_PATH = "storage/db.json";
 
 export let db;
 
 let writeQueued = false;
 
 export default {
+  init: () => {
+    const _db = new Low(new JSONFile(DB_PATH));
+    _db.read();
+    db = _db;
 
-	init: () => {
+    if (!db.data || Object.keys(db.data).length === 0) {
+      db.data = {
+        users: {},
+        guilds: {},
+        jnfr: {
+          pot: 0,
+          shop_date: null,
+        },
+      };
+      db.write();
+    }
 
-		console.log({cwd: process.cwd()});
-		fs.readdirSync(process.cwd()).forEach(file => {
-			console.log('- ' + file);
-		});
+    log.info("Database initialized!");
+  },
 
-		const _db = new Low(new JSONFile(DB_PATH));
-		_db.read();
-		db = _db;
+  getUser: (id) => {
+    if (!db.data.users[id]) {
+      db.data.users[id] = {
+        balance: 0,
+        inventory: [],
+        deaths: 0,
+        race: "Human",
+      };
+    }
+    return db.data.users[id];
+  },
 
-		if (!db.data || Object.keys(db.data).length === 0) {
-			db.data = {
-				users: {},
-				guilds: {},
-				jnfr: {
-					pot: 0,
-					shop_date: null
-				}
-			}
-			db.write();
-		}
+  getGuild: (id) => {
+    if (!db.data.guilds[id]) {
+      db.data.guilds[id] = {
+        pinboard: {},
+        emojiroles: [],
+      };
+    }
+    return db.data.guilds[id];
+  },
 
-		log.info('Database initialized!');
-	},
-
-	getUser: (id) => {
-		if (!db.data.users[id]) {
-			db.data.users[id] = {
-				balance: 0,
-				inventory: [],
-				deaths: 0,
-				race: 'Human'
-			};
-		}
-		return db.data.users[id];
-	},
-
-	getGuild: (id) => {
-		if (!db.data.guilds[id]) {
-			db.data.guilds[id] = {
-				pinboard:{},
-				emojiroles:[]
-			};
-		}
-		return db.data.guilds[id];
-	},
-
-	scheduleWrite: () => {
-		if (!writeQueued) {
-			log.info('Writing database contents in ',SCHEDULED_WRITE_TIMEOUT,'ms');
-			writeQueued = true;
-			setTimeout(() => {
-				db.write();
-				writeQueued = false;
-				log.info('Database contents written');
-			},SCHEDULED_WRITE_TIMEOUT)
-		}
-	}
-
-}
+  scheduleWrite: () => {
+    if (!writeQueued) {
+      log.info("Writing database contents in ", SCHEDULED_WRITE_TIMEOUT, "ms");
+      writeQueued = true;
+      setTimeout(() => {
+        db.write();
+        writeQueued = false;
+        log.info("Database contents written");
+      }, SCHEDULED_WRITE_TIMEOUT);
+    }
+  },
+};
