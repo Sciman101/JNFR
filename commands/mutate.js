04a7faff0672b23bc79a22fe7cb178888273d146
Sciman101 <Sciman1011@Gmail.com>
2024-01-05 23:10:49 -0500

    Added admin lock to commands

--- commands/mutate.js
+++ commands/mutate.js
@@ -1,17 +1,16 @@
-import { discordMention, stringValue } from '../parser/arguments.js';
-import Database, { db } from '../util/db.js';
-import { Permissions } from 'discord.js';
+import { discordMention, stringValue } from "../parser/arguments.js";
+import Database, { db } from "../util/db.js";
 
 export default {
-    name: 'mutate',
-    aliases: ['alter'],
-    description: 'Set a user\'s species',
-    argTree: discordMention('user', 'user', stringValue('race')),
-    permissions: [Permissions.FLAGS.MANAGE_MESSAGES],
-    guildOnly: true,
-    execute(message, args) {
-        const userData = Database.getUser(args.user);
-        userData.race = args.race;
-        return message.reply('User has been turned into a ' + args.race);
-    }
-}
+  name: "mutate",
+  aliases: ["alter"],
+  description: "Set a user's species",
+  argTree: discordMention("user", "user", stringValue("race")),
+  adminOnly: true,
+  guildOnly: true,
+  execute(message, args) {
+    const userData = Database.getUser(args.user);
+    userData.race = args.race;
+    return message.reply("User has been turned into a " + args.race);
+  },
+};
